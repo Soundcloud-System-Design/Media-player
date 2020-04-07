09018d669c619bde67b9c99c9ccc1f8d0e628492
Jake Knudsen <jakeknudsen@Jakes-MacBook-Pro.local>
2020-04-07 14:34:24 -0700

    deleted code that was not needed

--- server/database/seedDb.js
+++ server/database/seedDb.js
@@ -1,20 +1,6 @@
 const pool = require("./dbconnect.js");
 // // seed the DB with 10mil records
 
-// const songUrl = [
-//   "https://soundcloud-mp3.s3-us-west-1.amazonaws.com/Business+Men(3).wav",
-//   "https://soundcloud-mp3.s3-us-west-1.amazonaws.com/River+Road+bounce+1.mp3",
-//   "https://soundcloud-mp3.s3-us-west-1.amazonaws.com/The+News+(Final).wav",
-//   "https://soundcloud-mp3.s3-us-west-1.amazonaws.com/amanda(cut2).wav",
-//   "https://soundcloud-mp3.s3-us-west-1.amazonaws.com/bensound-dubstep.mp3",
-//   "https://soundcloud-mp3.s3-us-west-1.amazonaws.com/bensound-epic.mp3",
-//   "https://soundcloud-mp3.s3-us-west-1.amazonaws.com/bensound-happyrock.mp3",
-//   "https://soundcloud-mp3.s3-us-west-1.amazonaws.com/bensound-highoctane.mp3",
-//   "https://soundcloud-mp3.s3-us-west-1.amazonaws.com/bensound-punky.mp3",
-//   "https://soundcloud-mp3.s3-us-west-1.amazonaws.com/bensound-rumble.mp3",
-//   "https://soundcloud-mp3.s3-us-west-1.amazonaws.com/download.png",
-// ];
-
 pool.query(
   `COPY songs (song_name, song_url) FROM '/Users/jakeknudsen/Documents/SDC/Media-player/server/database/songs.csv' DELIMITER ',' CSV HEADER`,
   (err, res) => {
